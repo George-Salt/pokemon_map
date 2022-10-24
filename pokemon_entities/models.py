fb1ea63363194166cee7980a754dd1f36bd644ee
George Salt <solanikg3@gmail.com>
2022-10-24 16:00:01 +0300

    add verbose names to models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,20 +1,22 @@
+from tabnanny import verbose
 from django.db import models
 from django.utils import timezone
 
 
 class Pokemon(models.Model):
     id = models.AutoField(primary_key=True)
-    title_ru = models.CharField(max_length=200)
-    title_en = models.CharField(max_length=200, null=True, blank=True)
-    title_jp = models.CharField(max_length=200, null=True, blank=True)
-    description = models.TextField(null=True, blank=True)
-    image = models.ImageField(null=True, blank=True)
+    title_ru = models.CharField("Название на русском языке", max_length=200)
+    title_en = models.CharField("Название на английском языке", max_length=200, null=True, blank=True)
+    title_jp = models.CharField("Название на японском языке", max_length=200, null=True, blank=True)
+    description = models.TextField("Описание", null=True, blank=True)
+    image = models.ImageField("Изображение", null=True, blank=True)
     previous_evolution = models.ForeignKey(
         "self",
         on_delete=models.SET_NULL,
         null=True,
         blank=True,
-        related_name="next_evolution"
+        related_name="next_evolution",
+        verbose_name="Предыдущая эволюция", 
     )
 
     def __str__(self):
@@ -22,16 +24,16 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE)
-    lat = models.FloatField()
-    lon = models.FloatField()
-    appeared_at = models.DateTimeField(default=timezone.now)
-    disappeared_at = models.DateTimeField(default=timezone.now)
-    level = models.IntegerField(blank=True, null=True)
-    health = models.IntegerField(blank=True, null=True)
-    strength = models.IntegerField(blank=True, null=True)
-    defence = models.IntegerField(blank=True, null=True)
-    stamina = models.IntegerField(blank=True, null=True)
+    pokemon = models.ForeignKey(Pokemon, on_delete=models.CASCADE, verbose_name="Покемон, к которому относится")
+    lat = models.FloatField("Широта")
+    lon = models.FloatField("Долгота")
+    appeared_at = models.DateTimeField("С какого момента начнет действовать", default=timezone.now)
+    disappeared_at = models.DateTimeField("До какого момента будет действовать", default=timezone.now)
+    level = models.IntegerField("Уровень", blank=True, null=True)
+    health = models.IntegerField("Здоровье", blank=True, null=True)
+    strength = models.IntegerField("Атака", blank=True, null=True)
+    defence = models.IntegerField("Защита", blank=True, null=True)
+    stamina = models.IntegerField("Выносливость", blank=True, null=True)
         
     def __str__(self):
         return self.pokemon.title_ru
